09d224a8c8954b14203b871af2c657a5762d04e3
hiroyuki.iitsuka <iitsuka.info@gmail.com>
2020-03-21 07:21:43 +0000

    rails base files create

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+  
+  # アクション定義
+  
+  # Prefix:tasks Verb:GET  
+  def index
+    
+  end  
+  
+  # Prefix:none Verb:POST
+  def create
+    
+  end
+  
+  # Prefix:new_task Verb:GET
+  def new
+    
+  end
+  
+  # Prefix:edit_task Verb:GET
+  def edit
+    
+  end
+  
+  # Prefix:task Verb:GET
+  def show
+    
+  end
+  
+  
+  # Prefix:none Verb:PATCH(PUT)
+  def update
+    
+  end
+  
+  # Prefix:none Verb:DELETE  
+  def destroy
+    
+  end
+
+  #プライベート関数定義
+  private
+
+    
+end
